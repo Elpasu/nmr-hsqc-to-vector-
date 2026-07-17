b3e10dee97517dde21e0f456c12a07def6ae518d
Elpasu <passaglialucas95@gmail.com>
2026-07-17 17:32:02 -0300

    fix: corrige ruta de clon en cluster en scripts sbatch + restaura comentario
    experiments/D_val_congelado/run_eval.sh tenia ~/nmr-hsqc-to-vector (sin guion),
    inconsistente con el nombre real del repo clonado (nmr-hsqc-to-vector-, con
    guion, confirmado en el cluster). Los scripts de Exp B ya tenian la ruta
    correcta pero habian perdido el comentario "ajustar esta ruta" que Exp D
    si tenia. Unifica los tres scripts: ruta correcta + comentario en los tres.
    
    No afecta el job de Exp B ya corriendo (2375431), que se lanzo desde el
    directorio correcto.

--- experiments/B_regularizacion/run_eval.sh
+++ experiments/B_regularizacion/run_eval.sh
@@ -12,6 +12,7 @@
 source /home/lpassaglia.iquir/anaconda3/etc/profile.d/conda.sh
 conda activate /home/lpassaglia.iquir/anaconda3/envs/NMR_env
 
+# Ajustar esta ruta a donde hayas clonado el repo en el cluster.
 cd ~/nmr-hsqc-to-vector-/experiments/B_regularizacion
 
 python -u evaluate.py --config config.yaml --oraculo both --batch-size 256
--- experiments/B_regularizacion/run_train.sh
+++ experiments/B_regularizacion/run_train.sh
@@ -12,6 +12,7 @@
 source /home/lpassaglia.iquir/anaconda3/etc/profile.d/conda.sh
 conda activate /home/lpassaglia.iquir/anaconda3/envs/NMR_env
 
+# Ajustar esta ruta a donde hayas clonado el repo en el cluster.
 cd ~/nmr-hsqc-to-vector-/experiments/B_regularizacion
 
 python -u train.py --config config.yaml
