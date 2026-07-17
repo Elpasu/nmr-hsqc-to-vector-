a17148aed492d0461126fa8f3b4286b61cb0ca65
Elpasu <passaglialucas95@gmail.com>
2026-07-17 17:18:50 -0300

    exp B: run_train.sh (sbatch, --gres=gpu:1, 14h)

---
+++ experiments/B_regularizacion/run_train.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=expB_train
+#SBATCH --partition=gpua10_hi
+#SBATCH --output=expB_train_%j.out
+#SBATCH --error=expB_train_%j.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=16G
+#SBATCH --time=14:00:00
+#SBATCH --gres=gpu:1
+
+source /home/lpassaglia.iquir/anaconda3/etc/profile.d/conda.sh
+conda activate /home/lpassaglia.iquir/anaconda3/envs/NMR_env
+
+cd ~/nmr-hsqc-to-vector-/experiments/B_regularizacion
+
+python -u train.py --config config.yaml
