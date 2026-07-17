e62c85cc1196ecb4cb44d4fff3d8b390804d9f7d
Elpasu <passaglialucas95@gmail.com>
2026-07-17 17:20:37 -0300

    exp B: run_eval.sh (sbatch, --gres=gpu:1)

---
+++ experiments/B_regularizacion/run_eval.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=expB_eval
+#SBATCH --partition=gpua10_hi
+#SBATCH --output=expB_eval_%j.out
+#SBATCH --error=expB_eval_%j.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=16G
+#SBATCH --time=00:30:00
+#SBATCH --gres=gpu:1
+
+source /home/lpassaglia.iquir/anaconda3/etc/profile.d/conda.sh
+conda activate /home/lpassaglia.iquir/anaconda3/envs/NMR_env
+
+cd ~/nmr-hsqc-to-vector-/experiments/B_regularizacion
+
+python -u evaluate.py --config config.yaml --oraculo both --batch-size 256
