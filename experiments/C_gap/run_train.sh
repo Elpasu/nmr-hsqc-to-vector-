cf36f5001de5731f181ceab8a0a249c65b95fca1
Elpasu <passaglialucas95@gmail.com>
2026-07-20 11:31:39 -0300

    exp C: run_train.sh (sbatch, --gres=gpu:1, 14h)

---
+++ experiments/C_gap/run_train.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=expC_train
+#SBATCH --partition=gpua10_hi
+#SBATCH --output=expC_train_%j.out
+#SBATCH --error=expC_train_%j.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=16G
+#SBATCH --time=14:00:00
+#SBATCH --gres=gpu:1
+
+source /home/lpassaglia.iquir/anaconda3/etc/profile.d/conda.sh
+conda activate /home/lpassaglia.iquir/anaconda3/envs/NMR_env
+
+# Ajustar esta ruta a donde hayas clonado el repo en el cluster.
+cd ~/nmr-hsqc-to-vector-/experiments/C_gap
+
+python -u train.py --config config.yaml
